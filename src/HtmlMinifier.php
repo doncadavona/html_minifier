979d21b707c796f3dfd8051e423bd96ab77d44cf
Don Cadavona <dcadavona@gmail.com>
2017-06-25 17:37:12 +0000

    Initial minify function.

--- src/HtmlMinifier.php
+++ src/HtmlMinifier.php
@@ -5,7 +5,7 @@ namespace DonCadavona\HtmlMinifier;
 class HtmlMinifier
 {
     /**
-     * Create a new Skeleton Instance
+     * Create a new HtmlMinifier Instance
      */
     public function __construct()
     {
@@ -22,5 +22,36 @@ class HtmlMinifier
     public function echoPhrase($phrase)
     {
         return $phrase;
+    }
+
+    /**
+     * Returns a minified version of the given HTML.
+     * @param  string $html The original HTML.
+     * @return string       The minified version of the given HTML.
+     */
+    public function minify($html)
+    {
+        /**
+         * The set of regular expressions to match against
+         * the given HTML and their respective replacements.
+         * Reference: https://github.com/ogheo/yii2-htmlcompress
+         * @var array
+         */
+        $filters = [
+            // remove javascript comments
+            '/(?:<script[^>]*>|\G(?!\A))(?:[^\'"\/<]+|"(?:[^\\"]+|\\.)*"|\'(?:[^\\\']+|\\.)*\'|\/(?!\/)|<(?!\/script))*+\K\/\/[^\n|<]*/xsu' => '',
+            // remove html comments except IE conditions
+            '/<!--(?!\s*(?:\[if [^\]]+]|<!|>))(?:(?!-->).)*-->/su' => '',
+            // remove comments in the form /* */
+            '/\/+?\s*\*[\s\S]*?\*\s*\/+/u' => '',
+            // shorten multiple white spaces
+            '/>\s{2,}</u' => '><',
+            // shorten multiple white spaces
+            '/\s{2,}/u' => ' ',
+            // collapse new lines
+            '/(\r?\n)/u' => '',
+        ];
+
+        return preg_replace(array_keys($filters), array_values($filters), $html);
     }
 }
